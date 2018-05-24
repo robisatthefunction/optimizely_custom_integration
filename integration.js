59b1f63af0ecaa0f9fb6008137be295d5d9bfb67
Robert Saunders <rsaunders@us-rsaunders.local>
2018-05-24 18:47:19 -0500

    second

--- integration.js
+++ integration.js
@@ -1,7 +1,20 @@
-var decisionObject = window.optimizely.get('state').getDecisionObject({
+var state = window.optimizely.get('state');
+
+var decisionObject = state.getDecisionObject({
   campainId: campaignId,
   shouldCleanString: true
 });
 
+var allCampaigns = state.getCampaignStates({
+  "isActive": true
+})
+
+if (!allCampaigns[campaignId].isCampaignHoldback) {
+
 var experimentId = decision["experiment"].split(' ')[1].replace(/[()]/g, '');
-var variationName = decision["variation"].split(' ')[0]
+var variationName = decision["variation"].split(' ')[0];
+
+//Other integration code
+
+
+}
