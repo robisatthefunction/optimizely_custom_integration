19fdd1827279e52c7138c546e109e346903b4170
Robert Saunders <rsaunders@us-rsaunders.local>
2018-05-25 13:04:05 -0500

    fix

--- integration.js
+++ integration.js
@@ -1,20 +1,33 @@
+var campaignDecided = function(event) {
+
 var state = window.optimizely.get('state');
 
+console.log(event);
+var campaignId = event.data.campaign.id;
+console.log(campaignId);
 var decisionObject = state.getDecisionObject({
-  campainId: campaignId,
+  campaignId: campaignId,
   shouldCleanString: true
 });
 
-var allCampaigns = state.getCampaignStates({
-  "isActive": true
-})
+console.log(decisionObject);
 
-if (!allCampaigns[campaignId].isCampaignHoldback) {
+var experimentId = decisionObject["experiment"].split(' ')[1].replace(/[()]/g, '');
+var variationName = decisionObject["variation"].split(' ')[0];
 
-var experimentId = decision["experiment"].split(' ')[1].replace(/[()]/g, '');
-var variationName = decision["variation"].split(' ')[0];
+console.log(experimentId, variationName);
 
 //Other integration code
 
+};
 
-}
+window["optimizely"] = window["optimizely"] || [];
+window["optimizely"].push({
+  type: "addListener",
+  filter: {
+    type: "lifecycle",
+    name: "campaignDecided"
+  },
+  // Add the campaignDecided function as a handler.
+  handler: campaignDecided
+});
