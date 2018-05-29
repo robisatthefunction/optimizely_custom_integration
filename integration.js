3717d033314fda8fb69ca9823b8ceee0bb240b65
Robert Saunders <rsaunders@us-rsaunders.local>
2018-05-29 16:27:10 -0500

    fix

--- integration.js
+++ integration.js
@@ -1,16 +1,8 @@
-var campaignDecided = function(event) {
-
-var state = window.optimizely.get('state');
-
-console.log(event);
-var campaignId = event.data.campaign.id;
-console.log(campaignId);
-var decisionObject = state.getDecisionObject({
+var decisionObject = window.optimizely.get('state').getDecisionObject({
   campaignId: campaignId,
   shouldCleanString: true
 });
 
-//Make sure the user is not in the holdback
 if (decisionObject) {
 
 var experimentId = decisionObject["experiment"].split(' ')[1].replace(/[()]/g, '');
@@ -21,15 +13,3 @@ console.log(experimentId, variationName);
 //Other integration code
 
 };
-};
-
-window["optimizely"] = window["optimizely"] || [];
-window["optimizely"].push({
-  type: "addListener",
-  filter: {
-    type: "lifecycle",
-    name: "campaignDecided"
-  },
-  // Add the campaignDecided function as a handler.
-  handler: campaignDecided
-});
