6df912a31f5b22a27fee1bfba9dc7a957e87de29
Robert Saunders <rsaunders@us-rsaunders.us-east-1.optimizely>
2018-06-08 14:39:43 -0500

    added event for successful log to IQL

--- integration.js
+++ integration.js
@@ -1,15 +1,38 @@
-var decisionObject = window.optimizely.get('state').getDecisionObject({
-  campaignId: campaignId,
-  shouldCleanString: true
+/* This snippet runs everytime a "campaign decision" is made. */
+var utils = window.optimizely.get('utils');
+
+//Wait until the tk and rpcLogUrl variables are available. They are required for when we log
+utils.waitUntil(function() {
+  console.log("waiting for tk");
+  return window.tk;
+}).then(function() {
+  var decisionObject = window.optimizely.get('state').getDecisionObject({
+    campaignId: campaignId,
+    shouldCleanString: true
+  });
+
+  console.log(decisionObject);
+
+  //Check to see if a decision has been made. This will be null if the user is part of the holdback group
+  if (decisionObject) {
+    var experimentId = decisionObject.experiment.split(' ')[1].replace(/[()]/g, '');
+    var variationName = decisionObject.variation.split(' ')[0];
+
+    //Log to IQL
+    var tk = window.tk;
+    var baseUrl = "//gdc.indeed.com/rpc/log";
+
+    if (tk) {
+      var logUrl = baseUrl + '?a=thJsv&tk=' + tk;
+			logUrl += '&optExpIds=' + encodeURIComponent(experimentId);
+      logUrl += '&optVarNames=' + encodeURIComponent(variationName) + '_loggedFromOptimizelySnippet';
+      (new Image()).src = logUrl;
+      //fire event to Optimizely that the experiment data was logged to IQL
+      window.optimizely.push({
+        "type": "event",
+        "eventName": "integrationSuccess"
+      });
+      console.log("Logged: " + logUrl);
+    }
+  }
 });
-
-if (decisionObject) {
-
-var experimentId = decisionObject["experiment"].split(' ')[1].replace(/[()]/g, '');
-var variationName = decisionObject["variation"].split(' ')[0];
-
-console.log(experimentId, variationName);
-
-//Other integration code
-
-};
