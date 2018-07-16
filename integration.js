ea403d9451aa93627482aef38a6b0a20f7ee1799
Robert Saunders <rsaunders@us-rsaunders.local>
2018-07-16 15:57:40 -0500

    bare bones

--- integration.js
+++ integration.js
@@ -1,12 +1,4 @@
-/* This snippet runs everytime a "campaign decision" is made. */
-var utils = window.optimizely.get('utils');
-
-//Wait until the tk and rpcLogUrl variables are available. They are required for when we log
-utils.waitUntil(function() {
-  console.log("waiting for tk");
-  return window.tk;
-}).then(function() {
-  var decisionObject = window.optimizely.get('state').getDecisionObject({
+var decisionObject = window.optimizely.get('state').getDecisionObject({
     campaignId: campaignId,
     shouldCleanString: true
   });
@@ -18,21 +10,6 @@ utils.waitUntil(function() {
     var experimentId = decisionObject.experiment.split(' ')[1].replace(/[()]/g, '');
     var variationName = decisionObject.variation.split(' ')[0];
 
-    //Log to IQL
-    var tk = window.tk;
-    var baseUrl = "//gdc.indeed.com/rpc/log";
-
-    if (tk) {
-      var logUrl = baseUrl + '?a=thJsv&tk=' + tk;
-			logUrl += '&optExpIds=' + encodeURIComponent(experimentId);
-      logUrl += '&optVarNames=' + encodeURIComponent(variationName) + '_loggedFromOptimizelySnippet';
-      (new Image()).src = logUrl;
-      //fire event to Optimizely that the experiment data was logged to IQL
-      window.optimizely.push({
-        "type": "event",
-        "eventName": "integrationSuccess"
-      });
-      console.log("Logged: " + logUrl);
-    }
+    console.log(experimentId);
+    console.log(variationName);
   }
-});
