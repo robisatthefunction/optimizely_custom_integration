ec406b485a6c4b6a07ba3a511e6acb683ba73907
Robert Saunders <rsaunders@us-rsaunders.local>
2018-07-27 14:16:43 -0500

    example log endpoint added

--- integration.js
+++ integration.js
@@ -1,15 +1,26 @@
+/* This snippet runs everytime a "campaign decision" is made. */
 var decisionObject = window.optimizely.get('state').getDecisionObject({
-    campaignId: campaignId,
-    shouldCleanString: true
-  });
+  campaignId: campaignId,
+  shouldCleanString: true
+});
 
-  console.log(decisionObject);
 
-  //Check to see if a decision has been made. This will be null if the user is part of the holdback group
-  if (decisionObject) {
-    var experimentId = decisionObject.experiment.split(' ')[1].replace(/[()]/g, '');
-    var variationName = decisionObject.variation.split(' ')[0];
+//Check to see if a decision has been made. This will be null if the user is part of the holdback group
+if (decisionObject) {
+  var experimentId = decisionObject.experiment.split(' ')[1].replace(/[()]/g, '');
+  var variationName = decisionObject.variation.split(' ')[0];
 
-    console.log(experimentId);
-    console.log(variationName);
-  }
+  //Log to IQL
+  var baseUrl = "//www.example.com/cmsapi/logging/optimizely";
+
+  var logUrl = baseUrl + '?a=thJsv';
+  logUrl += '&optExpIds=' + encodeURIComponent(experimentId);
+  logUrl += '&optVarNames=' + encodeURIComponent(variationName) + '_snippetLogging';
+  (new Image()).src = logUrl;
+
+  /*//Back up Optimizely event to see if things work
+  window.optimizely.push({
+    "type": "event",
+    "eventName": "integrationSuccess"
+  });*/
+}
